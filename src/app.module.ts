8bd573b1a939e32a1325007b08b1f8b9873dcbd8
WumaCoder <1829913225@qq.com>
2021-09-22 11:50:33 +0800

    chore: change config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ModulesModule } from './users/modules/modules.module';
+import config from './config';
 
 @Module({
+  imports: [MikroOrmModule.forRoot(config().orm), ModulesModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
-import { Options } from '@mikro-orm/core';
 import { SqlHighlighter } from '@mikro-orm/sql-highlighter';
 import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
+import { MikroOrmModuleSyncOptions } from '@mikro-orm/nestjs';
 
 export default () => {
   return {
@@ -8,16 +8,17 @@ export default () => {
     orm: {
       type: 'mongo',
       host: e('MONGO_HOST', 'localhost'),
-      port: e('MONGO_PORT', 3306),
+      port: e('MONGO_PORT', 27017),
       user: e('MONGO_USER', 'root'),
-      password: e('MONGO_PASSWORD', '12345678'),
-      dbName: e('MONGO_DB_NAME', 'test'),
+      password: e('MONGO_PASSWORD', ''),
+      dbName: e('MONGO_DB_NAME', 'hello-nest'),
       entities: ['dist/**/*.entity.js'],
       entitiesTs: ['src/**/*.entity.ts'],
       debug: e('MONGO_DEBUG', false),
       highlighter: new SqlHighlighter(),
       metadataProvider: TsMorphMetadataProvider,
-    } as Options,
+      autoLoadEntities: true,
+    } as MikroOrmModuleSyncOptions,
     // more config ...
   };
 };
